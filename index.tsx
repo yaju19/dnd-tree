5de83621d308c1354b51cd3d1f2e670845549e91
yaju19 <82891380+yaju19@users.noreply.github.com>
2022-07-01 16:56:20 +0530

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,19 @@
+/**
+  This CodeSandbox has been automatically generated from the contents of https://bitbucket.org/atlassian/atlaskit-mk-2/raw/HEAD/packages/core/tree/examples/4-drag-and-drop.tsx.
+
+  This generator does not follow relative imports beyond those that reference the
+  module root, and as such, other relative imports may fail to load.
+
+  You can look up the relative imports from https://bitbucket.org/atlassian/atlaskit-mk-2
+
+  If this fails in any other way, contact Ben Conolly (https://bitbucket.org/bconolly)
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@atlaskit/css-reset';
+import Example from './example';
+
+ReactDOM.render(
+  <Example />,
+  document.getElementById('root')
+);
